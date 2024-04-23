5b5a89d456dca071539dd59f89b03232eafedad9
Waluigi15243 <81125222+Waluigi15243@users.noreply.github.com>
2024-04-22 20:41:09 -0400

    Update search_client.py

--- gRPC/search_client.py
+++ gRPC/search_client.py
@@ -2,9 +2,11 @@ import search_pb2
 import search_pb2_grpc
 import time
 import grpc
+import redis
 
 def run():
     with grpc.insecure_channel("localhost:50051") as channel:
+        r = redis.Redis(host='localhost', port=6379)
         stub = search_pb2_grpc.SearchCarsStub(channel)
         temp = input("Select a number from 1 to 23906: ")
         rpc_call = int(temp)
@@ -13,6 +15,7 @@ def run():
             car_request = search_pb2.GetCarRequest(rpc_call)
             car_reply = stub.GetCar(car_request)
             print(car_reply)
+            r.set(car_reply[0], car_reply[1])
         elif rpc_call < 1 or rpc_call > 23906:
             print("Número fuera de rango")
 
